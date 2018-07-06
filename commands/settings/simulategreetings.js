6df2a29dcce3afc51c5fff01b019f3fe3ef058fd
Unknown <funxgame1@gmail.com>
2018-07-06 16:06:29 +0200

    Address issue #33

--- commands/settings/simulategreetings.js
+++ commands/settings/simulategreetings.js
@@ -33,6 +33,10 @@ class SimulateGreetings extends Command {
         if (!guildEntry.greetings.channel || (guildEntry.greetings.channel !== 'dm' && !message.channel.guild.channels.has(guildEntry.greetings.channel))) {
             return message.channel.createMessage(':x: The greetings\'s message target is not set');
         }
+        //Backward compatibility, see issue #33 (https://github.com/ParadoxalCorp/felix-production/issues/33)
+        if (message.channel.guild.channels.get(guildEntry.greetings.channel).type !== 0) {
+            return message.channel.createMessage(':x: The greetings\'s message target is not a text channel, you should change it to a text channel in order for greetings to work');
+        }
         client.bot.emit('guildMemberAdd', message.channel.guild, message.channel.guild.members.get(message.author.id));
         return message.channel.createMessage(client.commands.get('setgreetings')._checkPermissions(client, message, guildEntry));
     }
