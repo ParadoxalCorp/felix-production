add005fe4308a64a808bb5fd43272428fd3d1125
Unknown <paradoxalcorp@gmail.com>
2018-08-13 10:25:43 +0200

    Add necessary lavalink stats for performance tracking

--- commands/admin/clientstats.js
+++ commands/admin/clientstats.js
@@ -26,6 +26,8 @@ class ClientStats extends Command {
         if (client.bot.uptime < 60000) {
             return message.channel.createMessage(`:x: Please wait another ${60000 - client.bot.uptime}ms`);
         }
+        const normalizeMemory = (memory) => `${(memory / 1024 / 1024).toFixed(2)}MB`;
+        const normalizeLoad = (load) => `${(load * 100).toFixed(2)}%`;
         await message.channel.createMessage({
             embed: {
                 title: ':gear: Client stats',
@@ -42,6 +44,26 @@ class ClientStats extends Command {
                     {
                         name: 'General stats',
                         value: `Guilds: ${client.stats.guilds} | Cached users: ${client.stats.users} | Large guilds: ${client.stats.largeGuilds}`
+                    },
+                    {
+                        name: 'Lavalink nodes',
+                        value: (() => {
+                            let nodesStatus = '```ini\n';
+                            for (const node of client.config.options.music.nodes) {
+                                const lavalinkNode = client.bot.voiceConnections.nodes.get(node.host);
+                                nodesStatus += `[${node.location}]: ${lavalinkNode.connected ? '[Online]' : '[Offline]'}\n`;
+                                nodesStatus += `=> Used memory: [${normalizeMemory(lavalinkNode.stats.memory.used)}]\n`;
+                                nodesStatus += `=> Allocated memory: [${normalizeMemory(lavalinkNode.stats.memory.allocated)}]\n`;
+                                nodesStatus += `=> Free memory: [${normalizeMemory(lavalinkNode.stats.memory.free)}]\n`;
+                                nodesStatus += `=> Cores: [${lavalinkNode.stats.cpu.cores}]\n`;
+                                nodesStatus += `=> System load: [${normalizeLoad(lavalinkNode.stats.cpu.systemLoad)}]\n`;
+                                nodesStatus += `=> Node load: [${normalizeLoad(lavalinkNode.stats.cpu.lavalinkLoad)}]\n`;
+                                nodesStatus += `=> Uptime: [${client.timeConverter.toElapsedTime(lavalinkNode.stats.uptime, true)}]\n`;
+                                nodesStatus += `=> Players: [${lavalinkNode.stats.players}]\n`;
+                                nodesStatus += `=> Paused players: [${lavalinkNode.stats.players - lavalinkNode.stats.playingPlayers}]\n`;
+                            }
+                            return nodesStatus + '```';
+                        })()
                     }
                 ],
                 color: client.config.options.embedColor
@@ -49,8 +71,9 @@ class ClientStats extends Command {
         });
         const clustersShardsStats = await client.IPCHandler.fetchShardsStats();
         return message.channel.createMessage('```ini\n' + client.stats.clusters.map(c => {
-            let clusterStats = `Cluster [${c.cluster}]: [${c.shards}] shard(s) | [${c.guilds}] guild(s) | [${c.ram.toFixed(2)}]MB RAM used | Up for [${client.timeConverter.toElapsedTime(c.uptime, true)}]\n`;
-            for (const shard of clustersShardsStats.find(cluster => cluster.clusterID === c.cluster).data) {
+            const cluster = clustersShardsStats.find(cl => cl.clusterID === c.cluster);
+            let clusterStats = `Cluster [${c.cluster}]: [${c.shards}] shard(s) | [${c.guilds}] guild(s) | [${c.ram.toFixed(2)}]MB RAM used | Up for [${client.timeConverter.toElapsedTime(c.uptime, true)}] | Music connections: [${cluster.data[0].musicConnections}]\n`;
+            for (const shard of cluster.data) {
                 clusterStats += `=> Shard [${shard.id}]: [${shard.guilds}] guild(s) | [${shard.status}] | ~[${shard.latency}]ms\n`;
             }
             return clusterStats;
