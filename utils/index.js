835da1453d377557c4ca5d0a29e1231d42950ec5
Unknown <paradoxalcorp@gmail.com>
2018-08-19 14:34:44 +0200

    update index

--- utils/index.js
+++ utils/index.js
@@ -8,7 +8,7 @@
  * @property {import("./timeConverter.js").TimeConverter} timeConverter Provides methods to parse UNIX timestamps
  * @property {import("./sleep.js").sleep} sleep Reproduce PHP's sleep function
  * @property {import("./getRandomNumber.js").getRandomNumber} getRandomNumber Get a random integer between the specified interval
- * @property {import("./utils.js")} utils Some utility methods 
+ * @property {import("./helpers.js")} helpers Some utility methods 
  * @property {import("./paginate.js").paginate} paginate Split an array into multiples, used for pagination purposes
  * @property {import("./traverse.js").traverse} traverse Traverse through a given object 
  * @property {import("./prompt.js").prompt} prompt Create a prompt in the command-line 
@@ -32,6 +32,6 @@ module.exports = (client) => {
         prompt: require('./prompt'),
         isWholeNumber: require('./isWholeNumber'),
         moduleIsInstalled: require('./moduleIsInstalled'),
-        utils: new(require('./utils'))(client)
+        helpers: new(require('./helpers'))(client)
     };
 };
