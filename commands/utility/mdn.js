38bb25b3bfb9a1d267192ec70578fe41e2cfb6de
Unknown <paradoxalcorp@gmail.com>
2018-08-25 19:02:24 +0200

    Add pagination to mdn command

--- commands/utility/mdn.js
+++ commands/utility/mdn.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require('axios').default;
 const UtilityCommands = require('../../structures/CommandCategories/UtilityCommands');
 
 class MDN extends UtilityCommands {
@@ -15,40 +15,46 @@ class MDN extends UtilityCommands {
     /** @param {import("../../structures/Contexts/UtilityContext")} context */
 
     async run(context) {
-        let args = context.message.content.split(/\s+/);
-        args.shift();
-        if (!args[0]) {
+        if (!context.args[0]) {
             return context.message.channel.createMessage(":x: You must specify something to search");
         }
-        let result = await axios.default.get(
-            `https://developer.mozilla.org/en-US/search.json?locale=en-US&q=${encodeURIComponent(args.join())}`, { headers: { 'Content-Type': 'application/json' } })
+        if (context.args.join(" ").length > 100) {
+            return context.message.channel.createMessage(":x: You can't search for something over 100 characters");
+        }
+        const result = await axios.get(
+            `https://developer.mozilla.org/en-US/search.json?locale=en-US&q=${encodeURIComponent(context.args.join(" "))}`, { headers: { 'Content-Type': 'application/json' } })
             .then(r => r.data);
-        if (!result.documents || !result.documents.length) {
+        if (!result.documents || !result.documents[0]) {
             return context.message.channel.createMessage(":x: Your search did not returned any result");
         }
-        let firstResult = result.documents[0];
-        return context.message.channel.createMessage({
-            embed: {
-                color: context.client.config.options.embedColor.generic,
-                title: "MDN",
-                url: "https://developer.mozilla.org/en/",
-                thumbnail: {
-                    url: "https://developer.cdn.mozilla.net/static/img/opengraph-logo.dc4e08e2f6af.png"
-                },
-                fields: [{
-                    name: "Search results",
-                    value: `Here's the results for [${context.args.join(" ")}]` + `(
-                                        https://developer.mozilla.org/en-US/search?locale=en-US&q=${encodeURIComponent(context.args.join())})`
-                }, {
-                    name: "**" + firstResult.title + "**",
-                    value: firstResult.excerpt
-                }],
-                timestamp: new Date(),
-                footer: {
-                    icon_url: context.client.bot.user.avatarURL,
-                    text: "MDN search"
+        result.documents = result.documents.map(document => {
+            return {
+                embed: {
+                    color: context.client.config.options.embedColor.generic,
+                    title: context.args.join().substr(0, 124),
+                    url: `https://developer.mozilla.org/en-US/search?locale=en-US&q=${encodeURIComponent(context.args.join(" "))}`,
+                    thumbnail: {
+                        url: "https://developer.cdn.mozilla.net/static/img/opengraph-logo.dc4e08e2f6af.png"
+                    },
+                    fields: [{
+                        name: document.title,
+                        value: `${document.excerpt.replace(/\<mark\>/gm, '`').replace(/\<\/mark\>/gm, '`').substr(0, 1000)}..`
+                    }, {
+                        name: 'Tags',
+                        value: document.tags.join(', ')
+                    }],
+                    timestamp: new Date(),
+                    footer: {
+                        icon_url: context.client.bot.user.avatarURL,
+                        text: "Showing page {index}/{length}"
+                    }
                 }
-            }
+            };
+        });
+        return context.client.handlers.InteractiveList.createPaginatedMessage({
+            channel: context.message.channel,
+            messages: result.documents,
+            userID: context.message.author.id
         });
     }
 }
