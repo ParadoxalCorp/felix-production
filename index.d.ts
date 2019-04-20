0d6be5957c1a3a07d747c818206fbeb0858b0a33
Niputi <peter.lyholm@outlook.com>
2019-04-20 16:45:30 +0200

    added process types

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    ADMINS : string,
+    PREFIX : string,
+    CODENAME : string,
+    DISCORD_TOKEN : string,
+    EMBED_COLOR :string,
+    DATABASE_URI: string,
+    NODE_ENV: "development" | "production",
+    PROCESS_GUILDSPERSHARDS: string,
+    PROCESS_SHARDS: string,
+    PROCESS_CUSTERS: string,
+  }
+}
