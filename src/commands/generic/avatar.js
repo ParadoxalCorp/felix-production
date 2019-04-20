5eb6ce62f9bc4cc0b7190ed32a18ea667c84cb1c
Niputi <peter.lyholm@outlook.com>
2019-04-20 19:47:42 +0200

    avatar cmd

---
+++ src/commands/generic/avatar.js
@@ -0,0 +1,26 @@
+const Command = require("../../structures/Command");
+
+module.exports = class Avatar extends Command {
+    constructor(client) {
+        super(client, async(ctx) => {
+            const user = await ctx.fetchUser()
+            const target = user || ctx.msg.author;
+            
+            return ctx.msg.channel.createMessage({
+                embed: ctx.genericEmbed({
+                    author: {
+                        name: `Requested by: ${ctx.msg.author.username}#${ctx.msg.author.discriminator}`,
+                        icon_url: ctx.msg.author.avatarURL
+                    },
+                    title: `Link to the avatar`,
+                    url: target.avatarURL.replace("size=128", "size=512") || target.defaultAvatarURL,
+                    image: {
+                        url: target.avatarURL.replace("size=128", "size=512") || target.defaultAvatarURL
+                    }
+                })
+            })
+        });
+        this.setName("avatar")
+            .setDescription("Shows the avatar of the user");
+    }
+};
